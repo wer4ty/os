2fec414a22d78c39b40e07bd7d22c5236cbea503
roma0152 <roma0152@bitbucket.org>
2018-03-24 21:32:27 +0300

    run improve

--- run.sh
+++ run.sh
@@ -1,2 +1,1 @@
-#!/bin/bash
-g++ -o shell *.cpp -std=c++11 -Wall -Who-vla -pedantic -march=core2 -Os -pipe -fstack-protector-all -g3 -Wl,-O,1,-z,combreloc
+g++ -o shell *.cpp -std=c++11 -Wall -Wno-vla -pedantic -march=core2 -Os -pipe -fstack-protector-all -g3 -Wl,-O,1,-z,combreloc
