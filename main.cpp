8359070ba5df080c695ee2a68a2e2726d976a869
roma0152 <roma0152@bitbucket.org>
2018-03-24 22:01:10 +0300

    create class Shell add basic functions

--- main.cpp
+++ main.cpp
@@ -5,16 +5,44 @@
 
 using namespace std;
 
+class Shell {
+	private:  
+		string path;
+		string command;
+
+	public:
+		Shell() {
+			path = "~/";
+			command = "";
+		}
+
+		~Shell() {}
+
+		void read() {
+			cout << "$_EBash "+path+ ": ";
+			getline(cin, command);
+			cout <<  command << endl; 
+		}
+
+		void eval(string command) {
+			if (command == "exit") { exit(0); }
+		}
+
+		void start() {
+			while (1) {
+				Shell::read();
+				Shell::eval(command);
+			}
+		}
+};
+
+
 int main() {
+	Shell* s = new Shell;
+	s->start();
 
-string command;
-
-	while(1) {
-	cout << "$_EBash ~/ex2:";
-	getline(cin, command);
-	cout <<  command << endl;
-
-	if (command == "exit") { break; }
-	}
+	//while(1) {}
+	
+	delete s;
 	return 0;
 }
